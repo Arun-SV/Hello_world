67b4f0976ddd0148f67ce9c90ccbf88d1ce1e71f
Arun-SV <arunssv1995@gmail.com>
2016-06-15 15:30:01 +0530

    Add files via upload

---
+++ CK_Print.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.io.*;
+
+class CK_Print
+{
+  public static void main(String[] args)
+  {
+    System.out.println("Hello World"); 
+    System.out.println("Hello World");
+    System.out.println("Hello World");
+    System.out.println("Hello World");
+    System.out.println("Hello World");
+  }
+}
