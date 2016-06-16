812a34f0403de5f8040b5116ece26261d88203b6
Arun-SV <arunssv1995@gmail.com>
2016-06-16 15:58:44 +0530

    Add files via upload

---
+++ CK_Oddnum_interval.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.io.*;
+class CK_Oddnum_interval
+{
+  public static void main(String[] args)
+  {
+    System.out.println("ENTER THE MINIMUM RANGE:");
+    Scanner s=new Scanner(System.in);
+    int n1=s.nextInt();
+    System.out.println("ENTER THE MAXIMUM RANGE:");
+    int n2=s.nextInt();
+    System.out.println("ODD NUMBERS ARE");
+     for(int i=n1;i<=n2;i++)
+     {
+         if(i%2!=0)
+           System.out.println(i);
+     }
+  }
+}
