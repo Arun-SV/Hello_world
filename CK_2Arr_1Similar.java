aa18f3243fd6d6900c28f0e175dc0f2153b56091
Arun-SV <arunssv1995@gmail.com>
2016-06-17 16:03:59 +0530

    Add files via upload

---
+++ CK_2Arr_1Similar.java
@@ -0,0 +1,48 @@
+import java.io.*;
+import java.util.*;
+class CK_2Arr_1Similar
+{
+ public static void main(String[] args)
+ {
+     System.out.println("ENTER NUMBER OF ELEMENTS IN ARRAY 1: ");
+     Scanner s=new Scanner(System.in);
+     int n=s.nextInt();
+     System.out.println("ENTER THE ELEMENTS: ");
+     int[] Arr1=new int[n];
+     for(int i=0;i<n;i++)
+     {
+        Arr1[i]=s.nextInt();
+     }
+     System.out.println("ENTER NUMBER OF ELEMENTS IN ARRAY 2: ");
+     int m=s.nextInt();
+     System.out.println("ENTER THE ELEMENTS: ");
+     int[] Arr2=new int[m];
+     for(int i=0;i<m;i++)
+     {
+         Arr2[i]=s.nextInt();
+     }
+      int[] Arr3=new int[m];
+      int k=0;
+     for(int i=0;i<n;i++)
+     {
+         for(int j=0;j<m;j++)
+         {
+             if(Arr1[i]==Arr2[j])
+               {
+                    Arr3[k]=Arr1[i];                   
+                    k++;
+               }   
+                  
+         }
+     }
+     System.out.println("ARRAY 3 ELEMENTS ARE:");
+     for(int o:Arr3)
+     System.out.println(o);
+     int sum=0;
+     for(int i=0;i<m;i++)
+     {
+       sum+=Arr3[i];
+     }
+     System.out.println("SUM IS: "+sum);
+ }
+}
